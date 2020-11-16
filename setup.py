4a93d5e4b07ac584ff7eecd5b5a9f69c9c7643ed
RoranBlackfire <memotoskbhat@gmail.com>
2020-11-16 22:09:02 +0530

    updated version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pdfcc",
-    version="0.0.1",
+    version="1.0.0",
     author="S Krishna Bhat",
     author_email="memotoskbhat@gmail.com",
     description="PDF cropper & compressor: removes unwanted noise from pdf and compresses them",
